90062a0a686fbb52c29cf5452ca1ea54fd04737d
jordan <jordan.r@escolar.ifrn.edu.br>
2023-10-19 16:36:06 -0300

    css-changes

--- core/views.py
+++ core/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index')
+    return render(request, 'index.html')
 
 def register(request):
-    return render(request, 'register')
+    return render(request, 'register.html')
 
 def login(request):
     return render(request, 'login.html')
--- sgce/urls.py
+++ sgce/urls.py
@@ -21,8 +21,9 @@ from core import views
 
 urlpatterns = [
    path('index', views.index, name='index'),
-   path('register', views.register, name='register')
-   path('', views.index, name='index'),
+
+   path('register', views.register, name='register'),
+   
    path('login', views.login, name='login'),
 ]
 
