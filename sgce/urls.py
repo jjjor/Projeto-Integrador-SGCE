c91914d02e2f0537087394d5c5bf30b558987b89
LucasSobreira <lucasdantassodreira@gmail.com>
2023-10-21 12:44:31 -0300

    att_pag login

--- sgce/urls.py
+++ sgce/urls.py
@@ -21,6 +21,7 @@ from core import views
 
 urlpatterns = [
    path('', views.index, name='index'),
+   path('index', views.index, name='index'),
    path('register', views.register, name='register'),
    path('login', views.login, name='login'),
    path('profile', views.profile, name='profile'),
